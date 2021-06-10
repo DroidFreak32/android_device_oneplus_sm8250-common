27debbd5d9f88964996e5c37ffc574fc9baf4e52
chandu078 <chandudyavanapelli03@gmail.com>
2021-06-10 20:13:47 +0530

    sm8250-common: Add OnePlus Dolby support
    * Thanks to Omkar Chandorkar for sepolicy.
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Co-Authored-By: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    
    sm8250-common: Hide Dolby Atmos from launcher
    
    * Thanks to HELLBOY017
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    
    sm8250-common: proprietary-files: Update Dolby blobs
    
    * From OnePlus9ROxygen_13.I04_OTA_0040_all_2105290149_3f45a99b8c1a43eb
    * Also remove AudioFX
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    
    sm8250-common: Remove audio postprocess effects
    
    Helps Dolby to work without play/pause media.
    
    sm8250-common: Update daxService
    
    sm8250-common: Update DaxUI
    
    *Also update dolby icon in DeviceExtras,
    *unpin dolby blobs and remove dolby stagefright blobs as dolby codec decoding isnt supported by aosp
    
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Co-Authored-By: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Change-Id: I3a201c04d2f419a4d5d6c50f8ed8d13638f15e84

--- common.mk
+++ common.mk
@@ -41,7 +41,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     OnePlusCameraOverlay \
-    OnePlusGalleryOverlay
+    OnePlusGalleryOverlay \
+    OPSoundTunerOverlay
 
 # VNDK
 PRODUCT_USE_PRODUCT_VNDK_OVERRIDE := true
