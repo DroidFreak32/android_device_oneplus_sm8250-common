5d608a132d68de4e6abb6d1ce27bafce722b301f
cjh1249131356 <cjh1249131356@gmail.com>
2022-03-10 15:26:48 +0300

    sm8250-common: Add OnePlus Camera support
    * Camera blobs moved to device tree.
    * keep pinned blobs in common vendor
      -- com.qti.chi.override.so
      -- vendor.qti.hardware.camera.postproc@1.0-service-impl.so
    
    Co-Authored-By: Chandra Chaganti <chandra.chaganti@hotmail.com>
    Co-Authored-By: chandu078 <chandudyavanapelli03@gmail.com>
    Co-Authored-By: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>
    Co-Authored-By: Vachounet <vachounet@live.fr>
    Co-Authored-By: LuK1337 <priv.luk@gmail.com>
    Co-Authored-By: dianlujitao <dianlujitao@gmail.com>
    Co-Authored-By: Lup Gabriel <gwolf2u.website@gmail.com>
    Co-Authored-By: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-Authored-By: Vaisakh Murali <vaisakhmurali@gmail.com>
    Co-Authored-By: firebird11 <hbgassel@gmail.com>
    Co-Authored-By: darkobas <darkobas@gmail.com>
    Co-Authored-By: Shrikara B <shrikara@codeaurora.org>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>
    Change-Id: If24490d0220e3bf0523a4ce51b35b1e628a302a2

--- common.mk
+++ common.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
 
+# Inherit OOS Camera & gallery packages
+$(call inherit-product, vendor/opcamera/config.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
@@ -33,6 +36,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+
+PRODUCT_PACKAGES += \
+    OnePlusCameraOverlay \
+    OnePlusGalleryOverlay
 
 # VNDK
 PRODUCT_USE_PRODUCT_VNDK_OVERRIDE := true
@@ -190,6 +197,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libcamera2ndk_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
@@ -318,7 +326,9 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    libhidltransport \
     libhidltransport.vendor \
+    libhwbinder \
     libhwbinder.vendor
 
 # HotwordEnrollement app permissions
