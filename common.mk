AICP/device_oneplus_sm8250-common

sm8250-common: Introduce charge control HAL
AICP Edits:
 ** rebrand kscope -> lineage to ease maintainability of hardware_lineage_interfaces

Signed-off-by: LibXZR <i@xzr.moe>
Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>
Change-Id: I7441eebc675a957a10138d5c448fc45c872883d0

common.mk
@@ -209,6 +209,10 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Charge control
+PRODUCT_PACKAGES += \
+    vendor.lineage.chgctrl@1.0-service.oneplus_kona
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     ftm_power_config.sh \

DroidFreak32/android_device_oneplus_sm8250-common

common.mk
@@ -195,6 +195,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Charge control
+PRODUCT_PACKAGES += \
+    vendor.lineage.chgctrl@1.0-service.oneplus
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     ftm_power_config.sh \